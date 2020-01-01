31e5c89c2ae65f0164b51e907640750beefe4ab8
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-01-01 01:25:21 +0100

    Remove exposed cred

--- js/firebase-stuff.js
+++ js/firebase-stuff.js
@@ -3,13 +3,7 @@ require('firebase/auth');
 require('firebase/firestore');
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAg7S6-WdJhc0QCX-aTeJT6ts8FNVXiIa0",
-  authDomain: "hng-first-task.firebaseapp.com",
-  databaseURL: "https://hng-first-task.firebaseio.com",
-  projectId: "hng-first-task",
-  storageBucket: "",
-  messagingSenderId: "380526995854",
-  appId: "1:380526995854:web:fcfd270b120ab0758a5cdf"
+  
 };
 
 firebase.initializeApp(firebaseConfig);
